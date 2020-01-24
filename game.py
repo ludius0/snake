af39b81222fac653af3c5f3a4d2fb1e47b44e40d
ludius <57571014+ludius0@users.noreply.github.com>
2020-01-24 23:17:17 +0100

    Still in progress

--- game.py
+++ game.py
@@ -1,59 +1,112 @@
-import pygame
-import math
-import random
+import pygame, math, random
+from pygame.locals import *
 
 display = 600
-rows = 15
+rows = 20
+d = display
+apple_pos_list = [0, 0, 0, 0, False]
+
 
 class snake():
 
-    def move():
+    def draw_snake(display, body):
+        position = display // rows
+        a, b = 0, 0
+        a += position
+        b += position
+        pygame.draw.rect(background, (0, 100, 255), (body[0], body[1], a, b))
+
+    def move(display, snake_direction, body):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
 
         keys = pygame.key.get_pressed()
 
         for key in keys:
 
             if keys[pygame.K_LEFT]:
+                snake_direction = "LEFT"
                 print("Left")
             elif keys[pygame.K_RIGHT]:
+                snake_direction = "RIGHT"
                 print("Right")
             elif keys[pygame.K_UP]:
+                snake_direction = "UP"
                 print("Up")
             elif keys[pygame.K_DOWN]:
+                snake_direction = "DOWN"
                 print("Down")
+
+
+        position = display // rows
+        x, y = 0, 0
+        x += position
+        y += position
+        
+
+        if body[0] <= 0:
+            body[0] = 0 
+        if body[1] <= 0:
+            body[1] = 0 
+
+        if snake_direction == "LEFT":
+            body[0] = body[0] - x
+        elif snake_direction == "RIGHT":
+            body[0] = body[0] + x
+        elif snake_direction == "UP":
+            body[1] = body[1] - y
+        elif snake_direction == "DOWN":
+            body[1] = body[1] + y
+
+
+        snake.draw_snake(display, body)
                          
-        redrawScreen()
+        redraw_screen(background)
+
+    def collision_apple():
+        snack = False
+
+    def collision_snake():
+        pass
 
 
 
-def redrawScreen():
-    global screen
+def redraw_screen(background):
     background.fill((60, 150, 100))
     
     grid(display, rows, background)
+    apple(display, background)
     
     screen.blit(background, (0, 0))
+#    screen.blit(snake, apple)
     pygame.display.flip()
 
 
-def grid(d, rows, background):
-    position = d // rows
-    x = 0
-    y = 0
-    # draw 20 times for x and y line
+def grid(display, rows, background):
+    position = display // rows
+    x, y = 0, 0
     for l in range(rows):
         x += position
         y += position
-        pygame.draw.line(background, (0, 0, 0), (x,0),(x,d))
-        pygame.draw.line(background, (0, 0, 0), (0,y),(d,y))
+        pygame.draw.line(background, (0, 0, 0), (x,0),(x,display))
+        pygame.draw.line(background, (0, 0, 0), (0,y),(display,y))
 
 
-def apple(d, screen):
-    x = random.randrange(0, d, rows)
-    pygame.draw.rect(screen, (0, 0, 255), (100, 100, 100, 100))
+def apple(display, background):
+    global apple_pos_list, snack
+    
+    if apple_pos_list[4] == False:
+        position = display // rows
+        x = random.randrange(0, display, position)
+        y = random.randrange(0, display, position)
+        a, b = 0, 0
+        a += position
+        b += position
+        apple_pos_list = [x, y, a, b, True] #generated positions are stored in pos_list
+    pygame.draw.rect(background, (220, 0, 0), (apple_pos_list[0], apple_pos_list[1], apple_pos_list[2], apple_pos_list[3]))
+    return apple_pos_list
 
 
 def main():
@@ -64,22 +117,36 @@ def main():
     pygame.display.set_caption('Snake')
     clock = pygame.time.Clock()
 
+
     # Fill background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((0, 0, 0))
+
+
+    # Game feature
+    snack = False
+    snake_direction = None
+    pos_list = []
+
+
+    # Generate body
+    position = display // rows
+    x = random.randrange(0, display, position)
+    y = random.randrange(0, display, position)
+    body = [0, 0]
+    body[0] = x
+    body[1] = y
     
-
     
+    while True:
+        # Initialise frames
+#        pygame.time.delay(40)
+        clock.tick(10)
 
-    while 1:
-        pygame.time.delay(40)
-        clock.tick(40)
-
-        apple(display, background)
-
-        snake.move()
-#        redrawScreen()
+        
+        
+        snake.move(display, snake_direction, body)
+        redraw_screen(background)
 
 
 if __name__ == "__main__": main()
