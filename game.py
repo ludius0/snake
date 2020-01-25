950ae1bca6d4acafd0d9486232361005e4603853
ludius <57571014+ludius0@users.noreply.github.com>
2020-01-25 16:59:33 +0100

    Still in progress.
    Now it even render body of snake (hilariously bad)

--- game.py
+++ game.py
@@ -2,22 +2,49 @@ import pygame, math, random
 from pygame.locals import *
 
 display = 600
-rows = 20
+rows = 30
 d = display
 apple_pos_list = [0, 0, 0, 0, False]
 snake_pos_list = []
-
+head = []
+body = []
+snake_direction = None
+score = 0
+body_lenght = 1
 
 class snake():
 
-    def draw_snake(body):
+    def draw_head():
+        global head
         position = display // rows
         a, b = 0, 0
         a += position
         b += position
-        pygame.draw.rect(background, (0, 100, 255), (body[0], body[1], a, b))
+        # Draw a head
+        pygame.draw.rect(background, (0, 100, 255), (head[0], head[1], a, b))
 
-    def move(display, snake_direction, body):
+    def draw_body():
+        global body, head
+
+        position = display // rows
+        a, b = 0, 0
+        a += position
+        b += position
+        
+        for i in range(body_lenght):
+            if snake_direction == "LEFT":
+                body[0] = body[0] + a
+            elif snake_direction == "RIGHT":
+                body[0] = body[0] - a
+            elif snake_direction == "UP":
+                body[1] = body[1] + b
+            elif snake_direction == "DOWN":
+                body[1] = body[1] - b
+            pygame.draw.rect(background, (0, 100, 255), (body[0], body[1], a, b))
+        
+
+    def move(background, head):
+        global snake_direction, body
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -43,50 +70,69 @@ class snake():
         y += position
         
 
-        if body[0] <= 0:
-            body[0] = 0 
-        if body[1] <= 0:
-            body[1] = 0 
+        if head[0] < 0:
+            head[0] = 0 
+        elif head[1] < 0:
+            head[1] = 0
+        elif head[0] > display:
+            head[0] = display
+        elif head[1] > display:
+            head[1] = display
 
+        body = [head[0], head[1]]
         if snake_direction == "LEFT":
-            body[0] = body[0] - x
+            head[0] = head[0] - x
+                
         elif snake_direction == "RIGHT":
-            body[0] = body[0] + x
+            head[0] = head[0] + x
+                
         elif snake_direction == "UP":
-            body[1] = body[1] - y
+            head[1] = head[1] - y
+                
         elif snake_direction == "DOWN":
-            body[1] = body[1] + y
+            head[1] = head[1] + y
 
 
-        snake.draw_snake(body)
+        snake.draw_head()
+        
+
+        snake.collision_apple()
                          
         redraw_screen(background)
 
-    def collision_apple():
-        snack = False
 
+    def collision_apple():
+        global apple_pos_list, head, score, body_lenght
+        if apple_pos_list[0] == head[0] and apple_pos_list[1] == head[1]:
+            print("You got an apple!")
+            apple_pos_list[-1] = False
+            apple(background)
+            score += 1
+            x = apple_pos_list[0]
+            y = apple_pos_list[1]
+            body_lenght += 1
+            
+            
+            
+            
     def collision_snake():
         pass
 
 
 
-def redraw_screen(background):
-#    position = display // rows
-#    x = random.randrange(0, display, position)
-#    y = random.randrange(0, display, position)
-#    body = [x, y]
+def redraw_screen(background): #redraw everything on the screen: background, grid, snake and apple
     background.fill((60, 150, 100))
     
-    grid(display, rows, background)
-    apple(display, background)
-    snake.draw_snake(body)
+    grid(background)
+    apple(background)
+    snake.draw_head()
+    snake.draw_body()
     
     screen.blit(background, (0, 0))
-#    screen.blit(snake, apple)
     pygame.display.flip()
 
 
-def grid(display, rows, background):
+def grid(background):   # Generate a grip in form of cubes
     position = display // rows
     x, y = 0, 0
     for l in range(rows):
@@ -96,10 +142,10 @@ def grid(display, rows, background):
         pygame.draw.line(background, (0, 0, 0), (0,y),(display,y))
 
 
-def apple(display, background):
+def apple(background):  # Generate snack
     global apple_pos_list, snack
     
-    if apple_pos_list[4] == False:
+    if apple_pos_list[-1] == False:
         position = display // rows
         x = random.randrange(0, display, position)
         y = random.randrange(0, display, position)
@@ -112,7 +158,7 @@ def apple(display, background):
 
 
 def main():
-    global screen, background
+    global screen, background, head, snack
     # Initialise screen
     pygame.init()
     screen = pygame.display.set_mode((display, display))
@@ -126,27 +172,27 @@ def main():
 
 
     # Game feature
-    snack = False
-    snake_direction = None
     pos_list = []
-
 
     # Generate body
     position = display // rows
     x = random.randrange(0, display, position)
     y = random.randrange(0, display, position)
-    body = [x, y]
+    head = [x, y]
+    
     
     
     while True:
         # Initialise frames
 #        pygame.time.delay(40)
         clock.tick(10)
-
         
         
-        snake.move(display, snake_direction, body)
+        
+        snake.move(background, head)
         redraw_screen(background)
 
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
+    print(f"You scored: {score}")
