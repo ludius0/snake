f28a13c1debffb63c11d350414718f25bf806984
ludius <57571014+ludius0@users.noreply.github.com>
2020-01-25 22:01:27 +0100

    Update unworking_and_abandoned_snake.py

--- unworking_and_abandoned_snake.py
+++ unworking_and_abandoned_snake.py
@@ -14,26 +14,6 @@ body_lenght = 1
 pos_head = []
 turns = {}
 
-class cube():
-    rows = 20
-    w = 500
-    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
-
-        
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
-
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
-
 class snake():
 
     def draw_head():
